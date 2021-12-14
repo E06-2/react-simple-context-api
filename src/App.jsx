9c8a0c683775cdb10c706621a97fd78fdf9e2d96
jsk900 <jsk900@yahoo.co.uk>
2021-12-14 11:20:02 +0100

    simple context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import { useContext } from 'react';
+
+import Count from './components/Count';
+import Counter from './components/Counter';
+
+import MyContext from './context/MyContext';
+
 import './App.css';
 
 const App = () => {
+  const context = useContext(MyContext);
+  console.log(context);
+  const { appHeading } = context;
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <h1>{appHeading.current}</h1>
+      <Count />
+      <Counter />
     </main>
   );
-}
+};
 
 export default App;
-
